ab0df284b8429e995609cf27d592552ca7559a9e
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-10-29 02:10:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  
+
+  static capitalize(inputString){
+    let string = inputString.split("");
+    string[0] = string[0].toUpperCase();
+    return string.join("");;
+  }
+
+  static titleize(inputString){
+    const exceptList = "the, a, an, but, of, and, for, at, by, from".split(", ");
+    let sentence = inputString.split(" ");
+    let result = sentence.map(words => {
+      if(!exceptList.includes(words) || words==sentence[0])
+      { 
+        return this.capitalize(words); 
+      }
+      else { return words; }
+    })
+    return result.join(" ");
+  }
+
+  static sanitize(inputString){
+    return inputString.replace( /[^A-Za-z0-9 '-]/g, '' )
+
+  }
+
 }
