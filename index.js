55e57c16764e1978245f579e756a8559c4746da6
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-11-05 00:29:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  static sanitize(string) {
+		return string.replace(/[^A-Za-z0-9-' ]/g, '');
+  }
+  
+  static titleize(string) {
+    let words = string.split(' ');
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newString = [];
+    for (const word of words){
+      if (words[0] === word){
+        newString.push(this.capitalize(word))
+      } else if (exceptions.includes(word)){
+        newString.push(word)
+      } else {
+        newString.push(this.capitalize(word))
+      }
+    }
+		return newString.join(' ') ;
+	}
 }
